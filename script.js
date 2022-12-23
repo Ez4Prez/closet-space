f23f3ad6459ee4fa2a6a2843e3e4fc91a50ff5a4
Ezra Mays Richards <ezra1almond@gmail.com>
2022-12-22 19:52:42 -0500

    add script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript";
+h2.textContent = "Come out with the old, get in with the new";
 document.querySelector("body").appendChild(h2);
+h2.addEventListener('mouseover', function () {h2.style.backgroundColor = "deeppink"})
+h2.addEventListener('mouseleave', function () {h2.style.backgroundColor = "darkblue"})
